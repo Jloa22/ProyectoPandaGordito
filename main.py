907a5e25837d97e31d467d150aaaff9063a45d9f
Loa <jfroilanlaurentecas1151@gmail.com>
2025-12-05 17:37:52 -0500

    delete unused main.py to fix Railway deploy

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from back.dijkstra import ejecutar_dijkstra
-
-app = FastAPI()
-
-@app.get("/recomendar/{familia}")
-def recomendar(familia: str):
-    return ejecutar_dijkstra(familia)
